122d058ffc37fb79e9d6b6e2194b0db178729637
Luis2115 <lmorochofebres@gmail.com>
2022-03-21 19:59:32 -0500

    Implementando el registro de los participantes

--- src/components/Registro/Registro.js
+++ src/components/Registro/Registro.js
@@ -1,19 +1,36 @@
 import React from "react";
 import { Form, Input, message, Button, Space } from "antd";
+// import { uploadFile } from "../../service/uploadFile";
+import { addplains } from "../../config/plans";
 
 export default function Registro() {
   const [form] = Form.useForm();
 
-  const onFinish = () => {
-    message.success("Submit success!");
+  const onFinish = async (evt) => {
+    message.success("Registro Exitoso!");
+    //console.log(evt);
+    const data = {
+      nombre: evt.nameLocal,
+      descripccion: evt.description,
+      url: evt.url,
+    };
+
+    const token = await addplains(data);
+
+    form.resetFields();
+
+    console.log(token);
   };
 
   const onFinishFailed = () => {
     message.error("Submit failed!");
   };
 
-  const generarURL = () => {
-    console.log("Pruebas");
+  const generarURL = (evt) => {
+    // console.log("Pruebas");
+    // let file = evt.target.files[0];
+    // console.log(file.name);
+    // uploadFile(file);
   };
 
   return (
@@ -25,7 +42,7 @@ export default function Registro() {
       autoComplete="off"
     >
       <Form.Item
-        name="name-local"
+        name="nameLocal"
         label="Nombre del local"
         rules={[{ required: true, message: "Por favor, Ingrese un nombre!" }]}
       >
@@ -53,7 +70,7 @@ export default function Registro() {
       <Form.Item>
         <Space>
           <Button type="primary" htmlType="submit">
-            Submit
+            Registrar
           </Button>
         </Space>
       </Form.Item>
