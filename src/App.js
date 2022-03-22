0e106d83413fdaa1a5d565b76ea21e40f7246f50
Luis2115 <lmorochofebres@gmail.com>
2022-03-21 19:57:48 -0500

    Limpiando codigo

--- src/App.js
+++ src/App.js
@@ -10,33 +10,40 @@ import {
 } from "./config/plans";
 import { gettoken } from "./config/mithelworks";
 import { getpointsanalitic, getaperturevote } from "./config/votacions";
-import Registro from "./components/Registro/Registro";
 
 function App() {
-  useEffect(async () => {
-    const token = await getaperturevote();
-    // ##################################### analitic a los participantes
-    // const token = await getpointsanalitic()
-    // ##################################### read a los participantes
-    // const token = await readplains()
-    // ##################################### listar a los participantes
-    // const token = await getplains()
-    // ##################################### insertar a un participante
-    // const token = await addplains({
-    //     "nombre": "hjshfkjdshfjksdhfksd",
-    //     "descripccion": "fsdhfjksdhfjksdhf",
-    //     "url": "dfhsdjhfkjsdhfksd"
-    // });
-    //######################################## actualizar al participante
-    // const token = await updateplains(15,{
-    //     "nombre": "Emprsa el mercadona",
-    //     "descripccion": "Empresa de venta de verduras y productos",
-    //     "url": "http://imagengenerica.com"
-    // });
-    //######################################## Eliminar al participante
-    // const token = await deleteplains(15);
-    console.log(token);
-  });
+  // useEffect(async () => {
+  //   //const token = await getaperturevote();
+  //   // ##################################### analitic a los participantes
+  //   // const token = await getpointsanalitic()
+  //   // ##################################### read a los participantes
+  //   // const token = await readplains()
+  //   // ##################################### listar a los participantes
+  //   // const token = await getplains()
+  //   // ##################################### insertar a un participante
+  //   // const token = await addplains({
+  //   //     "nombre": "hjshfkjdshfjksdhfksd",
+  //   //     "descripccion": "fsdhfjksdhfjksdhf",
+  //   //     "url": "dfhsdjhfkjsdhfksd"
+  //   // });
+  //   //######################################## actualizar al participante
+  //   // const token = await updateplains(15,{
+  //   //     "nombre": "Emprsa el mercadona",
+  //   //     "descripccion": "Empresa de venta de verduras y productos",
+  //   //     "url": "http://imagengenerica.com"
+  //   // });
+  //   //######################################## Eliminar al participante
+  //   // const token = await deleteplains(15);
+  //   //console.log(token);
+  // });
+
+  useEffect(() => {
+    (async () => {
+      const token = await gettoken();
+      //console.log(token);
+    })();
+  }, []);
+
   return <Admin />;
 }
 
