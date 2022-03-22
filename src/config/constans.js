381fd0081129bedb8bba01c07ee43e073c7e32d7
Luis2115 <lmorochofebres@gmail.com>
2022-03-21 20:01:32 -0500

    Agregando constantes para AWS

--- src/config/constans.js
+++ src/config/constans.js
@@ -1,2 +1,7 @@
 export const domain_api = "http://localhost:9000";
 export const token = "jdksdj";
+
+export const id = "AKIA35LFYU7NOTH32FPC";
+export const secretKey = "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";
+export const region = "us-east-2";
+export const buckupName = "noticias";
